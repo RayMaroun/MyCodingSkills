c01872d3cc8a9bed1e0a8bd668eefbfc62a0cbe2
RayMaroun <raymondmaroun15@gmail.com>
2023-04-12 17:02:02 +0300

    Added a new Java Class

---
+++ MyCodingSkills.java
@@ -0,0 +1,12 @@
+public class MyCodingSkills {
+    public static void main(String[] args) {
+        System.out.println("Java");
+        System.out.println("Python");
+        System.out.println("JavaScript");
+        System.out.println("HTML/CSS");
+        System.out.println("SQL");
+        System.out.println("C++");
+        System.out.println("Ruby");
+        System.out.println("Swift");
+    }
+}
